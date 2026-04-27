62b5d18cee002272eb12f25c0250ad3930e54a1a
Laura Pessine <laurapessine@outlook.com>
2026-04-27 08:34:57 -0300

    feat: adiciona suporte a escopos aninhados no analisador semântico e verifica declarações

--- src/main/java/br/ufscar/dc/compiladores/LASemanticoUtils.java
+++ src/main/java/br/ufscar/dc/compiladores/LASemanticoUtils.java
@@ -6,11 +6,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LASemanticoUtils {
-    // Lista para guardar os erros semânticos encontrados para imprimir
+    // Lista para guardar os erros semânticos encontrados
     public static List<String> errosSemanticos = new ArrayList<>();
 
     public static void adicionarErroSemantico(Token t, String mensagem) {
         int linha = t.getLine();
         errosSemanticos.add("Linha " + linha + ": " + mensagem);
+    }
+
+    // Verifica se o símbolo existe em qualquer um dos escopos abertos
+    public static boolean verificarSimbolo(Escopos escopos, String nome) {
+        for (TabelaDeSimbolos tabela : escopos.percorrerEscoposAninhados()) {
+            if (tabela.existe(nome)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
